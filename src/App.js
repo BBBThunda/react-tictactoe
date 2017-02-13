4df609b8a430e44a5aa36b3851340d9d7e67d08c
BBBThunda <bbbthunda@gmail.com>
2017-02-13 00:10:03 -0500

    Working prototype with history

--- src/App.js
+++ src/App.js
@@ -1,21 +1,152 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-class App extends Component {
+class Square extends React.Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square value={this.props.squares[i]}
+      onClick={() => this.props.onClick(i)} />;
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+class Game extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null)
+      }],
+      stepNumber: 0,
+      nextPlayer: 'X'
+    };
+  }
+
+  handleClick(i) {
+    const step = this.state.stepNumber;
+    const history = this.state.history.slice(0, step + 1);
+    const current = history[step];
+    const squares = current.squares.slice();
+
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.nextPlayer;
+    this.setState({
+      history: history.concat([{
+        squares: squares
+      }]),
+      stepNumber: step + 1,
+      nextPlayer: this.state.nextPlayer === 'X' ? 'O' : 'X'
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      nextPlayer: (step % 2) ? 'O' : 'X'
+    });
+  }
+
+  render() {
+    const history = this.state.history;
+    const step = this.state.stepNumber;
+    const current = history[step];
+    const winner = calculateWinner(current.squares);
+
+    let status;
+    if (winner) {
+      status = winner + ' WINS!!!';
+    } else if (isFull(current.squares)) {
+      status = 'DRAW!!!';
+    } else {
+      status = 'Next player: ' + this.state.nextPlayer;
+    }
+
+    const moves = history.map((step, move) => {
+      const desc = move ? 'Move #' + move : 'Begin';
+      return (
+        <li key={move}><a href="#" onClick={() => {this.jumpTo(move)}}>{desc}</a></li>
+      );
+    });
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board squares={current.squares} onClick={(i) => this.handleClick(i)} />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+function isFull(squares) {
+  for (let i = 0; i < squares.length; i++) {
+    if (!squares[i]) {
+      return false;
+    }
+  }
+  return true;
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+import './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <Game />,
+//   document.getElementById('root')
+// );
